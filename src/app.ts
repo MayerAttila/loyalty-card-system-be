db6334ef9cd006ced17c12114b5aa56052febb12
MayerAttila <mayerattila4@gmail.com>
2026-01-07 18:09:07 +0100

    added password field to Business and Employee models, update controllers and routes to handle password hashing, configure CORS in app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,20 @@
 import express from "express";
+import cors from "cors";
 import { errorMiddleware } from "./common/errors/errorMiddleware";
 import { userRouter } from "./modules/user/user.routes";
 import { businessRouter } from "./modules/business/business.router";
 import { employeeRouter } from "./modules/employee/employee.router";
 
 export const app = express();
+
+app.use(
+  cors({
+    origin: "http://172.27.128.1:3000",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 
--- src/modules/business/business.conteroller.ts
+++ src/modules/business/business.conteroller.ts
@@ -1,9 +1,18 @@
 import type { Request, Response } from "express";
 import { prisma } from "../../prisma/client";
-import { get } from "node:http";
+import bcrypt from "bcryptjs";
 
 export const getAllBusinesses = async (req: Request, res: Response) => {
-  const businesses = await prisma.business.findMany();
+  const businesses = await prisma.business.findMany({
+    select: {
+      id: true,
+      name: true,
+      address: true,
+      email: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
 
   res.json(businesses);
 };
@@ -11,15 +20,32 @@ export const getBusinessById = async (req: Request, res: Response) => {
   const { id } = req.params;
   const business = await prisma.business.findUnique({
     where: { id },
+    select: {
+      id: true,
+      name: true,
+      address: true,
+      email: true,
+      createdAt: true,
+      updatedAt: true,
+    },
   });
 
   res.json(business);
 };
 
 export const createBusiness = async (req: Request, res: Response) => {
-  const { name, address, email } = req.body;
+  const { name, address, email, password } = req.body;
+  const hashed = await bcrypt.hash(password, 10);
   const business = await prisma.business.create({
-    data: { name, address, email },
+    data: { name, address, email, password: hashed },
+    select: {
+      id: true,
+      name: true,
+      address: true,
+      email: true,
+      createdAt: true,
+      updatedAt: true,
+    },
   });
 
   res.status(201).json(business);
